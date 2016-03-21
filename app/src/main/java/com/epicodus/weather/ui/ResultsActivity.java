df3ca01a9b5d84e581e164c95df857ab74e39b5b
kcmdouglas <kcmdouglas@gmail.com>
2016-03-21 14:22:43 -0700

    Add initial stylings to results activity.

--- app/src/main/java/com/epicodus/weather/ui/ResultsActivity.java
+++ app/src/main/java/com/epicodus/weather/ui/ResultsActivity.java
@@ -1,15 +1,27 @@
 package com.epicodus.weather.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.epicodus.weather.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class ResultsActivity extends AppCompatActivity {
+    @Bind(R.id.introResultText) TextView mIntroText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+
+        mIntroText.setText("Here's how the weather is looking in " + location);
     }
 }
