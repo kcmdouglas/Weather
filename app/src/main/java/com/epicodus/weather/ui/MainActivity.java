a3a022837987bf344f419ab538daa2189cb7ed65
kcmdouglas <kcmdouglas@gmail.com>
2016-03-21 13:19:07 -0700

    Add routing and subpackages

--- app/src/main/java/com/epicodus/weather/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/weather/ui/MainActivity.java
@@ -1,18 +1,20 @@
-package com.epicodus.weather;
+package com.epicodus.weather.ui;
 
-import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.epicodus.weather.R;
 import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.background_main) ImageView mBackground;
     @Bind(R.id.locationButton) Button mLocationButton;
     @Bind(R.id.locationEditText) EditText mLocationEditText;
@@ -27,5 +29,15 @@ public class MainActivity extends AppCompatActivity {
                 .fit()
                 .centerCrop()
                 .into(mBackground);
+        mLocationButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == mLocationButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
